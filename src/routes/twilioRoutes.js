8e51b9537b9415dee40dc790361bd2fe0089c283
Marcos Souza <marcos.souza@fcamara.com.br>
2020-05-03 14:12:26 -0300

    Adjusted the method to return token

--- src/routes/twilioRoutes.js
+++ src/routes/twilioRoutes.js
@@ -4,6 +4,6 @@ const controller = require ('../controllers/twilioController');
 
 router.get('/:room', controller.getTwilioRoom);
 router.post('/', controller.createTwilioRoom);
-router.get('/token', controller.createTwilioRoom);
+router.get('/token', controller.authToken);
 
 module.exports = router;
